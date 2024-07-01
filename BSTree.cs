c5a568c58f3c0ea1b1d85e3ac9225b880f27c067
ahmedyassercomp@gmail.com <ahmedyassercomp@gmail.com>
2024-07-01 20:53:26 +0300

    upload BSTree code and The main class

---
+++ BSTree.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject
+{
+    internal class BSTNode
+    {
+        public int data;
+
+        public BSTNode left;
+
+        public BSTNode right;
+
+        public BSTNode(int _data, BSTNode _left, BSTNode _right)
+        {
+            data = _data;
+            left = _left;
+            right = _right;
+        }
+
+        public BSTNode(int _data) : this(_data, null, null)
+        { }
+
+
+    }
+    internal class BSTree
+    {
+        private BSTNode root;
+
+        public BSTree()
+        {
+            root = null;
+        }
+
+        public void insert(BSTNode node)
+        {
+            if (root == null)
+            {
+                root = node;
+                return;
+            }
+
+            BSTNode curr = root;
+            while (curr != null)
+            {
+                if (curr.data >= node.data)
+                {
+                    if (curr.left != null)
+                        curr = curr.left;
+                    else
+                    {
+                        curr.left = node;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (curr.right != null)
+                        curr = curr.right;
+                    else
+                    {
+                        curr.right = node;
+                        break;
+                    }
+                }
+
+
+            }
+
+        }
+        public void insert(int data)
+        {
+            insert(new BSTNode(data));
+        }
+
+        #region delete and it's sub function
+        public void delete(int val)
+        {
+            if (root == null)
+                return;
+
+            if (root.data == val)
+            {
+                root = helper(root);
+                return;
+            }
+
+            BSTNode curr = root;
+            while (curr != null)
+            {
+                if (curr.data > val)
+                {
+                    if (curr.left != null && curr.left.data == val)
+                    {
+                        curr.left = helper(curr.left);
+                        break;
+                    }
+                    else
+                    {
+                        curr = curr.left;
+                    }
+                }
+                else
+                {
+                    if (curr.right != null && curr.right.data == val)
+                    {
+                        curr.right = helper(curr.right);
+                        break;
+                    }
+                    else
+                    {
+                        curr = curr.right;
+                    }
+                }
+            }
+            
+        }
+        private BSTNode helper(BSTNode node) 
+        {
+            if (node.left == null)
+                return node.right;
+            if (node.right == null)
+                return node.left;
+            BSTNode rightChild = node.right;
+            BSTNode lastRight = findLastRight(node.left);
+            lastRight.right = rightChild;
+            return node.left;
+        }
+        private BSTNode findLastRight(BSTNode node)
+        {
+            if (node.right == null)
+                return node;
+            return findLastRight(node.right);
+        }
+        #endregion
+
+        private bool search(int val,BSTNode node)
+        {
+            if (node == null)
+                return false;
+            if (node.data == val)
+                return true;
+            else if (node.data > val)
+                return search(val, node.left);
+            else
+                return search(val, node.right);
+        }
+        public bool search(int val)
+        {
+            return search(val, root);
+        }
+
+
+        public List<List<int>> BFS()
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            Queue<BSTNode> q = new Queue<BSTNode>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                List<int> level = new List<int>();
+                int size = q.Count;
+                for (int i = 0;i < size; i++)
+                {
+                    BSTNode node = q.Peek();
+                    q.Dequeue();
+
+                    if (node.left != null) q.Enqueue(node.left);
+                    if (node.right != null) q.Enqueue(node.right);
+                    level.Add(node.data);
+                }
+                result.Add(level);
+            }
+
+            return result;
+        }
+
+        public void printBST()
+        {
+            List<List<int>> result = BFS();
+
+            for (int i = 0;i < result.Count;i++)
+            {
+                for (int j = 0;j < result[i].Count;j++)
+                {
+                    Console.Write($"{result[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ HelloW.cs
@@ -0,0 +1,184 @@
+﻿using HelloWorldProject;
+using System;
+using System.Diagnostics.Metrics;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlTypes;
+
+namespace HWNS
+{
+    public delegate int StringFuncDelDT(string s);
+    class employee
+    {
+        int id;
+        string name;
+        long phoneNumber;
+
+        public employee(int id, string name, long pn)
+        {
+            this.id = id;
+            this.name = name;
+            phoneNumber = pn;
+        }
+
+        public long this[int id, string name]
+        {
+            get
+            {
+                if (this.id == id && this.name == name)
+                    return phoneNumber;
+                return -1;
+            }
+            set
+            {
+                this.phoneNumber = value;
+            }
+        }
+
+        public string this[int id, long pn]
+        {
+            get
+            {
+                if (this.id == id && this.phoneNumber == pn)
+                    return name;
+                return "Not Valid";
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int this[string name, long pn]
+        {
+            get
+            {
+                if (this.name == name && this.phoneNumber == pn)
+                    return this.id;
+                return -1;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+
+    }
+
+    class stringFunctions
+    {
+        public char ch;
+
+        public static int CountUpperCase(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+                if (char.IsUpper(s[i])) count++;
+            return count;
+        }
+        public static int getLengh(string s)
+        {
+            return s.Length;
+        }
+
+        public static int FullName(string fName, string lName)
+        {
+            return fName.Length + lName.Length;
+        }
+
+        public int CountChar(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ch) count++;
+            }
+            return count;
+        }
+
+    }
+
+
+
+    class Program
+    {
+        /*
+         * public static int prec(char c)
+        {
+            if (c == '^')
+                return 3;
+            else if (c == '/' || c == '*')
+                return 2;
+            else if (c == '+' || c == '-')
+                return 1;
+            else
+                return -1;
+        }
+        public static string infixToPostfix(string expression)
+        {
+            Stack<char> st = new Stack<char>();
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+                {
+                    sb.Append(c);
+                }
+                else if (c == '(')
+                {
+                    st.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (st.Peek() != '(')
+                    {
+                        sb.Append(st.Peek());
+                        st.Pop();
+                    }
+                    st.Pop();
+                }
+                else
+                {
+                    while (st.Count > 0 && prec(c) <= prec(st.Peek()))
+                    {
+                        sb.Append(st.Peek());
+                        st.Pop();
+                    }
+                    st.Push(c);
+                }
+            }
+
+            while (st.Count > 0)
+            {
+                sb.Append(st.Peek());
+                st.Pop();
+            }
+            string res = sb.ToString();
+            return res;
+        }
+         */
+        public static void Main()
+        {
+            BSTree bSTree = new BSTree();
+            bSTree.insert(7);
+            bSTree.insert(4);
+            bSTree.insert(3);
+            bSTree.insert(9);
+            bSTree.insert(8);
+            Console.WriteLine("before deleting 9: ");
+            bSTree.printBST();
+            Console.WriteLine("-----------------------");
+            bSTree.delete(9);
+            Console.WriteLine("after deleting 9");
+            bSTree.printBST();
+
+        }
+
+    }
+}
+
+            
+
+       
